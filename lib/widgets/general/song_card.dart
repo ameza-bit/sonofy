7d7a8c5ebe2b81a4a2857691187e578bd9260c70
Armando MacBP <ameza@bit.lat>
2025-03-03 10:19:09 -0600

    Agregar botón de reproducción personalizado en SongCard con diseño mejorado

--- lib/widgets/general/song_card.dart
+++ lib/widgets/general/song_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sonofy/widgets/player/play_button.dart';
 import 'package:sonofy/widgets/player/song_info.dart';
 
 class SongCard extends StatelessWidget {
@@ -7,6 +6,42 @@ class SongCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget playButton = Container(
+      width: 48,
+      height: 48,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+        border: Border.all(color: Colors.purple, width: 2),
+      ),
+      child: Icon(
+        Icons.play_arrow,
+        color: Colors.purple,
+        size: 32,
+      ),
+    );
+
+    if (true) {
+      playButton = Container(
+        width: 48,
+        height: 48,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: LinearGradient(
+            colors: [Colors.deepPurple, Colors.purple],
+          ),
+        ),
+        child: IconButton(
+          icon: const Icon(
+            Icons.pause,
+            color: Colors.white,
+            size: 28,
+          ),
+          onPressed: () {},
+        ),
+      );
+    }
+
     return Container(
       width: double.infinity,
       padding: const EdgeInsets.all(12),
@@ -18,7 +53,7 @@ class SongCard extends StatelessWidget {
       child: Row(
         spacing: 12,
         children: [
-          PlayButton(size: 60),
+          playButton,
           Expanded(child: SongInfo(isBottomSheet: true)),
           Text(
             '2:46',
