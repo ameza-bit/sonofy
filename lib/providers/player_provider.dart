a608464b94033b3d9c32746fc0b2396015c6e689
Armando Mini <ameza@bit.lat>
2025-03-07 22:36:21 -0600

    Agregar soporte para reproducción de audio con audioplayers y configurar el reproductor en PlayerProvider

--- lib/providers/player_provider.dart
+++ lib/providers/player_provider.dart
@@ -1,3 +1,12 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-class PlayerProvider extends ChangeNotifier {}
+class PlayerProvider extends ChangeNotifier {
+  AssetSource source = AssetSource('assets/music/wasteland.mp3');
+  final player = AudioPlayer();
+
+  void play() {
+    player.play(source);
+    notifyListeners();
+  }
+}
--- lib/widgets/player/play_button.dart
+++ lib/widgets/player/play_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sonofy/providers/player_provider.dart';
 
 class PlayButton extends StatelessWidget {
   const PlayButton({this.size = 80, super.key});
@@ -7,6 +9,8 @@ class PlayButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    PlayerProvider player = context.read<PlayerProvider>();
+
     return Container(
       width: size,
       height: size,
@@ -18,7 +22,9 @@ class PlayButton extends StatelessWidget {
       ),
       child: IconButton(
         icon: const Icon(Icons.play_arrow, size: 40, color: Colors.white),
-        onPressed: () {},
+        onPressed: () {
+          player.play();
+        },
       ),
     );
   }
