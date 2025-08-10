b59bdde8a7a340eefeb4c60139adf11e89f145a4
Armando Mini <ameza@bit.lat>
2025-08-10 09:27:25 -0600

    feat: Agregar modal de letras y actualizar la interacción en PlayerLyrics

--- lib/presentation/widgets/player/player_lyrics.dart
+++ lib/presentation/widgets/player/player_lyrics.dart
@@ -5,6 +5,7 @@ import 'package:sonofy/core/extensions/theme_extensions.dart';
 import 'package:sonofy/presentation/blocs/settings/settings_cubit.dart';
 import 'package:sonofy/presentation/blocs/settings/settings_state.dart';
 import 'package:sonofy/presentation/widgets/common/font_awesome/font_awesome_flutter.dart';
+import 'package:sonofy/presentation/widgets/player/lirycs_modal.dart';
 
 class PlayerLyrics extends StatelessWidget {
   const PlayerLyrics({super.key});
@@ -15,31 +16,34 @@ class PlayerLyrics extends StatelessWidget {
       builder: (context, state) {
         final primaryColor = state.settings.primaryColor;
 
-        return SizedBox(
-          width: double.infinity,
-          child: ClipRRect(
-            borderRadius: const BorderRadius.vertical(
-              top: Radius.circular(100.0),
-            ),
-            child: Material(
-              color: Theme.of(context).cardColor,
-              child: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(
-                        FontAwesomeIcons.lightChevronUp,
-                        color: primaryColor,
-                        size: 12,
-                      ),
-                      Text(
-                        context.tr('player.lyrics'),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: context.scaleText(12)),
-                      ),
-                    ],
+        return GestureDetector(
+          onTap: () => LyricsModal.show(context),
+          child: SizedBox(
+            width: double.infinity,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(100.0),
+              ),
+              child: Material(
+                color: Theme.of(context).cardColor,
+                child: SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.lightChevronUp,
+                          color: primaryColor,
+                          size: 12,
+                        ),
+                        Text(
+                          context.tr('player.lyrics'),
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: context.scaleText(12)),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
