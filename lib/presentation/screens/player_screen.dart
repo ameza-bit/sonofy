2eb35836f1ba87e294c831b5adfe323d0039a823
Armando Mini <ameza@bit.lat>
2025-08-10 01:26:23 -0600

    feat: Eliminar la pantalla de inicio de sesión y reestructurar la pantalla del reproductor con nuevos componentes

--- lib/presentation/screens/player_screen.dart
+++ lib/presentation/screens/player_screen.dart
@@ -1,8 +1,13 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sonofy/core/extensions/color_extensions.dart';
+import 'package:sonofy/core/extensions/theme_extensions.dart';
+import 'package:sonofy/presentation/widgets/common/font_awesome/font_awesome_flutter.dart';
 import 'package:sonofy/presentation/widgets/library/bottom_clipper_container.dart';
-import 'package:sonofy/presentation/widgets/player/player_app_bar.dart';
-import 'package:sonofy/presentation/widgets/player/player_body.dart';
+import 'package:sonofy/presentation/widgets/player/player_control.dart';
 import 'package:sonofy/presentation/widgets/player/player_lyrics.dart';
+import 'package:sonofy/presentation/widgets/player/player_slider.dart';
 
 class PlayerScreen extends StatelessWidget {
   static const String routeName = 'player';
@@ -14,7 +19,32 @@ class PlayerScreen extends StatelessWidget {
 
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: const PlayerAppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        foregroundColor: context.musicWhite,
+        actionsPadding: const EdgeInsets.only(right: 12.0),
+        leading: IconButton(
+          icon: const Icon(FontAwesomeIcons.lightChevronLeft, size: 20.0),
+          onPressed: () => context.pop(),
+        ),
+        title: Text(
+          context.tr('player.now_playing').toUpperCase(),
+          style: TextStyle(
+            fontSize: context.scaleText(16),
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(FontAwesomeIcons.lightHeart, size: 20.0),
+            onPressed: () {
+              // Show more options
+            },
+          ),
+        ],
+      ),
       body: Stack(
         children: [
           Image.network(
@@ -24,12 +54,51 @@ class PlayerScreen extends StatelessWidget {
             fit: BoxFit.fitHeight,
             colorBlendMode: BlendMode.darken,
           ),
+          Container(
+            width: double.infinity,
+            height: mediaQuery.size.height * 0.6,
+            color: context.musicDeepBlack.withValues(alpha: 0.5),
+          ),
           Column(
             children: [
               SizedBox(height: mediaQuery.size.height * 0.45),
-              const BottomClipperContainer(
-                padding: EdgeInsets.symmetric(horizontal: 28, vertical: 24),
-                child: PlayerBody(),
+              BottomClipperContainer(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 28,
+                  vertical: 24,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const PlayerSlider(),
+                    const SizedBox(height: 16),
+                    Text(
+                      'Different world of music',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: context.musicDeepBlack,
+                        fontSize: context.scaleText(20),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Manage your library settings and preferences.',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: context.scaleText(12),
+                        color: context.musicDarkGrey,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    const PlayerControl(),
+                  ],
+                ),
               ),
             ],
           ),
