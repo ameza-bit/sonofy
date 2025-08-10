c669050ae3bff69961bc51e3c7c722b944732663
Armando Mini <ameza@bit.lat>
2025-08-10 12:26:30 -0600

    feat: Actualizar configuraciones de Gradle, permisos de Android y mejorar la gestión de canciones

--- lib/data/repositories/songs_repository_impl.dart
+++ lib/data/repositories/songs_repository_impl.dart
@@ -1,9 +1,34 @@
+import 'package:on_audio_query_pluse/on_audio_query.dart';
 import 'package:sonofy/data/models/song.dart';
 import 'package:sonofy/domain/repositories/songs_repository.dart';
 
 final class SongsRepositoryImpl implements SongsRepository {
+  final OnAudioQuery _audioQuery = OnAudioQuery();
+
+  Future<bool> _configureAudioQuery() async {
+    await _audioQuery.setLogConfig(
+      LogConfig(logType: LogType.DEBUG, showDetailedLog: true),
+    );
+
+    return _checkAndRequestPermissions();
+  }
+
+  Future<bool> _checkAndRequestPermissions({bool retry = false}) async {
+    return _audioQuery.checkAndRequest(retryRequest: retry);
+  }
+
   @override
   Future<List<Song>> getSongsFromDevice() async {
+    final bool canContinue = await _configureAudioQuery();
+
+    if (!canContinue) {
+      return [];
+    }
+
+    await _audioQuery.querySongs().then((value) {
+      print('Canciones encontradas: ${value.length}');
+    });
+
     return [Song.empty(), Song.empty(), Song.empty()];
   }
 }
