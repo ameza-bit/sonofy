67b374761fc713cae6d706086f6a5dd7ede7cca2
Armando Mini <ameza@bit.lat>
2025-08-15 19:38:07 -0600

    feat: Agregar modelo de Playlist y simplificar la gestión de permisos en SongsRepositoryImpl

--- lib/data/repositories/songs_repository_impl.dart
+++ lib/data/repositories/songs_repository_impl.dart
@@ -1,5 +1,3 @@
-import 'dart:io' show Platform;
-
 import 'package:on_audio_query_pluse/on_audio_query.dart';
 import 'package:sonofy/domain/repositories/songs_repository.dart';
 
@@ -21,39 +19,6 @@ final class SongsRepositoryImpl implements SongsRepository {
   @override
   Future<List<SongModel>> getSongsFromDevice() async {
     final bool canContinue = await _configureAudioQuery();
-
-    if (!canContinue || Platform.isIOS) {
-      final dummySong = SongModel({
-        'title': 'Permission Denied',
-        'artist': 'Unknown',
-        'album': 'Unknown',
-        'duration': 0,
-        'file_extension': 'unknown',
-        'is_alarm': false,
-        'is_audiobook': false,
-        'is_music': false,
-        'is_notification': false,
-        'is_podcast': false,
-        'is_ringtone': false,
-      });
-      return [
-        dummySong,
-        dummySong,
-        dummySong,
-        dummySong,
-        dummySong,
-        dummySong,
-        dummySong,
-        dummySong,
-        dummySong,
-        dummySong,
-        dummySong,
-        dummySong,
-        dummySong,
-        dummySong,
-      ];
-    }
-
-    return _audioQuery.querySongs();
+    return !canContinue ? [] : _audioQuery.querySongs();
   }
 }
