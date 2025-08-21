f420c90a3ce6cb21c1e8e48c534fccd2fee40590
Armando Air <ameza@bit.lat>
2025-08-21 15:12:21 -0600

    fix: Actualizar traducciones de 'eliminar' a 'quitar' en opciones de canción y mejorar la gestión de eliminación de canciones de listas de reproducción

--- lib/presentation/widgets/options/options_modal.dart
+++ lib/presentation/widgets/options/options_modal.dart
@@ -1,7 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:sonofy/presentation/blocs/player/player_state.dart';
-import 'package:sonofy/presentation/blocs/songs/songs_state.dart';
 import 'package:sonofy/presentation/views/modal_view.dart';
 import 'package:sonofy/presentation/widgets/common/section_card.dart';
 import 'package:sonofy/presentation/widgets/options/add_playlist_option.dart';
@@ -9,7 +7,6 @@ import 'package:sonofy/presentation/widgets/options/create_playlist_option.dart'
 import 'package:sonofy/presentation/widgets/options/delete_playlist_option.dart';
 import 'package:sonofy/presentation/widgets/options/equalizer_option.dart';
 import 'package:sonofy/presentation/widgets/options/order_option.dart';
-import 'package:sonofy/presentation/widgets/options/remove_playlist_option.dart';
 import 'package:sonofy/presentation/widgets/options/rename_playlist_option.dart';
 import 'package:sonofy/presentation/widgets/options/reorder_option.dart';
 import 'package:sonofy/presentation/widgets/options/settings_option.dart';
@@ -25,7 +22,6 @@ import 'package:sonofy/presentation/widgets/options/remove_from_playlist_option.
 import 'package:on_audio_query_pluse/on_audio_query.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sonofy/presentation/blocs/player/player_cubit.dart';
-import 'package:sonofy/presentation/blocs/songs/songs_cubit.dart';
 
 class OptionsModal {
   BuildContext context;
@@ -56,38 +52,13 @@ class OptionsModal {
     const SettingsOption(),
   ]);
 
-  void player() {
-    final playerState = context.read<PlayerCubit>().state;
-    final songsState = context.read<SongsCubit>().state;
-
-    final options = <Widget>[
-      const SleepOption(),
-      const AddPlaylistOption(),
-      // Solo mostrar RemovePlaylistOption si estamos reproduciendo desde una playlist
-      // (no desde la biblioteca general que incluye todas las canciones)
-      if (_isPlayingFromPlaylist(playerState, songsState))
-        const RemovePlaylistOption(),
-      const SpeedOption(),
-      const EqualizerOption(),
-      const SettingsOption(),
-    ];
-    _show(options);
-  }
-
-  static bool _isPlayingFromPlaylist(
-    PlayerState playerState,
-    SongsState songsState,
-  ) {
-    // Si no hay canción seleccionada, no estamos reproduciendo desde playlist
-    if (!playerState.hasSelectedSong) return false;
-
-    // Si la cantidad de canciones en la playlist actual es menor que
-    // el total de canciones en la biblioteca, estamos en una playlist específica
-    final playlistLength = playerState.playlist.length;
-    final totalSongsLength = songsState.songs.length;
-
-    return playlistLength < totalSongsLength;
-  }
+  void player() => _show([
+    const SleepOption(),
+    const AddPlaylistOption(),
+    const SpeedOption(),
+    const EqualizerOption(),
+    const SettingsOption(),
+  ]);
 
   void songLibraryContext(SongModel song, List<SongModel> playlist) {
     final playerState = context.read<PlayerCubit>().state;
